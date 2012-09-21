a17995f971a45019ae74ba91c2e86f9a463aee02
Nadim Kobeissi <nadim@nadim.cc>
2012-09-21 00:05:59 -0400

    Add translations

--- crypto.cat/js/getCryptocat.js
+++ crypto.cat/js/getCryptocat.js
@@ -1,5 +1,6 @@
 $('.input[title]').qtip();
 $('.button[title]').qtip();
+var language = Language.set('en');
 
 function dialogBox(data, closeable, onClose) {
 	if ($('#dialogBox').css('top') !== '-450px') {
@@ -28,53 +29,58 @@ function dialogBox(data, closeable, onClose) {
 	});
 }
 
-var supportedText = 'Cryptocat easily installs in your browser for quick access whenever you need it.';
-var unsupportedText = 'Cryptocat is currently only available for <a href="https://google.com/chrome" target="_blank">Google Chrome</a> and <a href="http://getfirefox.com" target="_blank">Mozilla Firefox</a>. Switch to one of the world\'s leading browsers today to use Cryptocat.';
-
-var downloadButton = [];
-downloadButton['chrome'] = '<a href="#"><div id="downloadButton">'
-	+ '<img id="browserLogo" src="img/chrome.png" alt="Google Chrome" />'
-	+ '<h1>Get Cryptocat for Google Chrome</h1>'
-	+ '</div>'
-	+ '</a><div class="downloadText">' + supportedText + '</div>';
-downloadButton['firefox'] = '<a href="#"><div id="downloadButton">'
-	+ '<img id="browserLogo" src="img/firefox.png" alt="Mozilla Firefox" />'
-	+ '<h1>Get Cryptocat for Mozilla Firefox</h1>'
-	+ '</div>'
-	+ '</a><div class="downloadText">' + supportedText + '</div>';
-downloadButton['opera'] = '<a href="#"><div id="downloadButton">'
-	+ '<img id="browserLogo" src="img/opera.png" alt="Google Chrome" />'
-	+ '<h1>Your Browser is Not Supported.</h1>'
-	+ '</div>'
-	+ '</a><div class="downloadText">' + unsupportedText + '</div>';
-downloadButton['safari'] = '<a href="#"><div id="downloadButton">'
-	+ '<img id="browserLogo" src="img/safari.png" alt="Google Chrome" />'
-	+ '<h1>Your Browser is Not Supported.</h1>'
-	+ '</div>'
-	+ '</a><div class="downloadText">' + unsupportedText + '</div>';
-downloadButton['ie'] = '<a href="#"><div id="downloadButton">'
-	+ '<img id="browserLogo" src="img/ie.png" alt="Google Chrome" />'
-	+ '<h1>Your Browser is Not Supported.</h1>'
-	+ '</div>'
-	+ '</a><div class="downloadText">' + unsupportedText + '</div>';
-
-if (navigator.userAgent.match('Chrome')) {
+function getDownloadButton() {
+	var downloadButton = [];
+	downloadButton['chrome'] = '<a href="#"><div id="downloadButton" dir="ltr">'
+		+ '<img id="browserLogo" src="img/chrome.png" alt="Google Chrome" />'
+		+ '<p>' + language['mainWindow']['getChrome'] + '</p>'
+		+ '</div>'
+		+ '</a><div class="downloadText">' + language['mainWindow']['supportedText'] + '</div>';
+	downloadButton['firefox'] = '<a href="#"><div id="downloadButton">'
+		+ '<img id="browserLogo" src="img/firefox.png" alt="Mozilla Firefox" />'
+		+ '<h1>' + language['mainWindow']['getFirefox'] + '</h1>'
+		+ '</div>'
+		+ '</a><div class="downloadText">' + language['mainWindow']['supportedText'] + '</div>';
+	downloadButton['opera'] = '<a href="#"><div id="downloadButton">'
+		+ '<img id="browserLogo" src="img/opera.png" alt="Google Chrome" />'
+		+ '<h1>' + language['mainWindow']['notSupported'] + '</h1>'
+		+ '</div>'
+		+ '</a><div class="downloadText">' + language['mainWindow']['unsupportedText'] + '</div>';
+	downloadButton['safari'] = '<a href="#"><div id="downloadButton">'
+		+ '<img id="browserLogo" src="img/safari.png" alt="Google Chrome" />'
+		+ '<h1>' + language['mainWindow']['notSupported'] + '</h1>'
+		+ '</div>'
+		+ '</a><div class="downloadText">' + language['mainWindow']['unsupportedText'] + '</div>';
+	downloadButton['ie'] = '<a href="#"><div id="downloadButton">'
+		+ '<img id="browserLogo" src="img/ie.png" alt="Google Chrome" />'
+		+ '<h1>' + language['mainWindow']['notSupported'] + '</h1>'
+		+ '</div>'
+		+ '</a><div class="downloadText">' + language['mainWindow']['unsupportedText'] + '</div>';
+	if (navigator.userAgent.match('Chrome')) {
 	$('#download').html(downloadButton['chrome']);
+	}
+	else if (navigator.userAgent.match('Firefox')) {
+		$('#download').html(downloadButton['firefox']);
+		$('#browserLogo').css('bottom', '8px');
+	}
+	else if (navigator.userAgent.match('Opera')) {
+		$('#download').html(downloadButton['opera']);
+		$('#browserLogo').css('bottom', '11px');
+		$('#downloadButton').css('cursor', 'default');
+	}
+	else if (navigator.userAgent.match('Internet Explorer')) {
+		$('#download').html(downloadButton['ie']);
+		$('#downloadButton').css('cursor', 'default');
+	}
+	else {
+		$('#download').html(downloadButton['safari']);
+		$('#downloadButton').css('cursor', 'default');
+	}
 }
-else if (navigator.userAgent.match('Firefox')) {
-	$('#download').html(downloadButton['firefox']);
-	$('#browserLogo').css('bottom', '8px');
-}
-else if (navigator.userAgent.match('Opera')) {
-	$('#download').html(downloadButton['opera']);
-	$('#browserLogo').css('bottom', '11px');
-	$('#downloadButton').css('cursor', 'default');
-}
-else if (navigator.userAgent.match('Internet Explorer')) {
-	$('#download').html(downloadButton['ie']);
-	$('#downloadButton').css('cursor', 'default');
-}
-else {
-	$('#download').html(downloadButton['safari']);
-	$('#downloadButton').css('cursor', 'default');
-}
+
+// Language selector
+$('#languages').change(function() {
+	language = Language.set($(this).val());
+	getDownloadButton();
+});
+getDownloadButton();
---
+++ crypto.cat/js/language.js
@@ -0,0 +1,101 @@
+﻿var Language = function() {};
+(function(){
+
+var language = {};
+var defaultLanguage;
+
+Language.setDefault = function(lang) {
+	defaultLanguage = language[lang];
+}
+
+Language.set = function(lang) {
+	lang = language[lang];
+	$('body').css('font-family', lang['font-family']);
+	$('html').attr('dir', lang['direction']);
+	$('#introHeader').html(lang['mainWindow']['introHeader']);
+	$('#introParagraph1').html(lang['mainWindow']['introParagraph1']);
+	$('#introParagraph2').html(lang['mainWindow']['introParagraph2']);
+	$('#project').html(lang['mainWindow']['project']);
+	$('#blog').html(lang['mainWindow']['blog']);
+	$('#source').html(lang['mainWindow']['source']);
+	return lang;
+}
+
+language.en = {
+	'language': 'en',
+	'direction': 'ltr',
+	'font-family': 'Helvetica, Arial, Verdana',
+	'mainWindow': {
+		'introHeader': '﻿Private Conversations for Everyone.',
+		'introParagraph1': 'Cryptocat is an instant messaging platform that lets you easily have private conversations with friends. Messages are encrypted before leaving your screen and are protected from being viewed by any third party, even from us.',
+		'introParagraph2': 'Cryptocat is free software built on open standards. Our development process is under continuous examination by a community of volunteers and enthusiasts. <a href="https://project.crypto.cat" target="_blank">Learn more about the Cryptocat Project</a>.',
+		'project': 'Project',
+		'blog': 'Blog',
+		'source': 'Source',
+		'supportedText': 'Cryptocat easily installs in your browser for quick access whenever you need it.',
+		'unsupportedText': 'Cryptocat is currently only available for <a href="https://google.com/chrome" target="_blank">Google Chrome</a> and <a href="http://getfirefox.com" target="_blank">Mozilla Firefox</a>. Switch to one of the world\'s leading browsers today to use Cryptocat.',
+		'getChrome': 'Get Cryptocat for Google Chrome',
+		'getFirefox': 'Get Cryptocat for Mozilla Firefox',
+		'notSupported': 'Your Browser is Not Supported.'
+	}
+}
+
+language.ca = {
+	'language': 'ca',
+	'direction': 'ltr',
+	'font-family': 'Helvetica, Arial, Verdana',
+	'mainWindow': {
+		'introHeader': '﻿Converses privades per a tothom',
+		'introParagraph1': 'Cryptocat es una plataforma de missatgeria instantània que et permet mantenir converses privades fàcilment amb els amics. Els missatges es xifren abans de sortir de la teva pantalla i són protegits de ser vistos per terceres persones, incloses nosaltres.',
+		'introParagraph2': 'Cryptocat és free software construït sobre estàndards oberts. El nostre procés de desenvolupament està en període d\'evaluació continuada per part d\'una comunitat de voluntaris i entusiastes. <a href="https://project.crypto.cat" target="_blank">Descobreix més sobre el Projecte Cryptocat</a>.',
+		'project': 'Projecte',
+		'blog': 'Bloc',
+		'source': 'Font',
+		'supportedText': 'Cryptocat easily installs in your browser for quick access whenever you need it.',
+		'unsupportedText': 'Cryptocat is currently only available for <a href="https://google.com/chrome" target="_blank">Google Chrome</a> and <a href="http://getfirefox.com" target="_blank">Mozilla Firefox</a>. Switch to one of the world\'s leading browsers today to use Cryptocat.',
+		'getChrome': 'Get Cryptocat for Google Chrome',
+		'getFirefox': 'Get Cryptocat for Mozilla Firefox',
+		'notSupported': 'Your Browser is Not Supported.'
+	}
+}
+
+language.fr = {
+	'language': 'fr',
+	'direction': 'ltr',
+	'font-family': 'Helvetica, Arial, Verdana',
+	'mainWindow': {
+		'introHeader': '﻿Conversations Privées Pour Tout le Monde.',
+		'introParagraph1': 'Cryptocat est une plateforme de messagerie instantanée qui vous permet facilement d\'avoir des conversations privées avec des amis. Les messages sont cryptés avant de quitter votre écran et sont protégés contre leur lecture par un tiers, même de nous.',
+		'introParagraph2': 'Cryptocat est un logiciel libre construit sur ​​des standards ouverts. Notre processus de développement est en cours d\'examen continu par une communauté de bénévoles et de passionnés. <a href="https://project.crypto.cat" target="_blank">Apprenez plus sur le projet Cryptocat</a>.',
+		'project': 'Projet',
+		'blog': 'Blog',
+		'source': 'Source',
+		'supportedText': 'Cryptocat s\'installe facilement dans votre navigateur pour un accès rapide chaque fois que vous en avez besoin.',
+		'unsupportedText': 'Cryptocat est actuellement disponible pour <a href="https://google.com/chrome" target="_blank">Google Chrome</a> et <a href="http://getfirefox.com" target="_blank">Mozilla Firefox</a>. Installez un des principaux navigateurs du monde d\'aujourd\'hui pour utiliser Cryptocat.',
+		'getChrome': 'Obtenez Cryptocat pour Google Chrome',
+		'getFirefox': 'Obtenez Cryptocat pour Mozilla Firefox',
+		'notSupported': 'Votre Navigateur n\'est pas Supporté.'
+	}
+}
+
+language.ar = {
+	'language': 'ar',
+	'direction': 'rtl',
+	'font-family': 'Tahoma, DejaVu, Helvetica, Arial, Verdana',
+	'mainWindow': {
+		'introHeader': '﻿محادثات خصوصية وآمنة لجميع الأشخاص.',
+		'introParagraph1': 'كريبتوكات هو برنامج للتراسل اللحظي، يتيح لك القيام بالمحادثات الآمنة والخصوصية مع أصدقائك وذلك من خلال تعمية (تشفير) الرسائل قبل مغادرتها لشاشة حاسوبك فتبقى محميّة من التعرض للإختراق من قبل الآخرين أو حتى نحن.',
+		'introParagraph2': 'كريبتوكات هو برنامج مجاني مبني على معايير مفتوحة. عملية تطويره لا تزال تحت البحث والدراسة المستمرة من قبل مجموعة من المتطوعين والمتحمسين. اعرف المزيد عن مشروع كريبتوكات.',
+		'project': 'مشروع',
+		'blog': 'مدونة',
+		'source': 'مصدر',
+		'supportedText': 'كريبتوكات يتثبت بسهولة في متصفحك لإستعماله بسرعة الحاجة.',
+		'unsupportedText': 'كريبتوكات حالياً ليس ملائم إلا مع Google Chrome و-Mozilla Firefox. سجل احدهم لإستعمال كريبتوكات.',
+		'getChrome': 'إحصل على كريبتوكات ل-Google Chrome',
+		'getFirefox': 'إحصل على كريبتوكات ل-Mozilla Firefox',
+		'notSupported': 'متصفحك ليس ملائم مع كريبتوكات.'
+	}
+}
+
+
+})();
