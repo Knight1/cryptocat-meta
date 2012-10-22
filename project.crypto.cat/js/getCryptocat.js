11bf69a903688606559854d62b10f40cf7605921
Nadim Kobeissi <nadim@nadim.cc>
2012-10-22 11:50:21 -0400

    Cool new media quotes ticker!

--- project.crypto.cat/js/getCryptocat.js
+++ project.crypto.cat/js/getCryptocat.js
@@ -4,6 +4,31 @@
 	'safari': 'https://crypto.cat/get/cryptocat.safariextz'
 }
 
+var mediaQuotes = [
+	'“Cryptocat has a simple, countercultural goal: people should be able to talk on the Internet without being subjected to surveillance.” - The New York Times',
+	'“Cryptocat is deceptively simple for a web app that can save lives, subvert governments and frustrate marketers.” - Wired',
+	'“The beauty of Cryptocat is its simplicity.” - PC World',
+	'“One of the best Google Chrome apps currently out there.” - Business Insider',
+	'“We’ve finally found a stylish and secure way to chat online that fits our hip/paranoid lifestyle: Cryptocat.” - Netted',
+	'“16th Annual Webby Awards Honoree, Social Media Category.” - Webby Awards',
+	'“Wall Street Journal Data Transparency Award for Outstanding Data Control Project.” - Wall Street Journal',
+	'“Cryptocat brings extra-secure communication to web chat, especially in places where conversations might be watched.” - Ars Technica',
+	'“Cryptocat may turn out to be the Web’s easiest way to communicate with strong encryption.” - Forbes',
+	'“Cryptocat creates an encrypted, disposable chatroom on any computer with a web browser.” - Lifehacker',
+	'“Cryptocat works inside a web browser and enables people to chat online via encrypted instant messaging.” - BBC News'
+];
+currentQuote = 0;
+function switchQuote() {
+	if (currentQuote >= mediaQuotes.length) {
+		currentQuote = 0;
+	}
+	$('#mediaQuote p').fadeOut('slow', function() {
+		$(this).text(mediaQuotes[currentQuote]);
+		currentQuote++;
+		$(this).fadeIn('slow');
+	});
+}
+
 var language = {
 		'getChrome': 'Get Cryptocat for Google Chrome',
 		'getFirefox': 'Get Cryptocat for Mozilla Firefox',
@@ -56,3 +81,5 @@ function getDownloadButton() {
 }
 
 getDownloadButton();
+setInterval('switchQuote()', 8000);
+switchQuote();
