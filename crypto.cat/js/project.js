38f96c776defb3bb7a22b2387a79fb64ffc8462f
Nadim Kobeissi <nadim@nadim.cc>
2013-01-26 14:27:09 -0500

    Clean up JS

--- crypto.cat/js/project.js
+++ crypto.cat/js/project.js
@@ -16,40 +16,60 @@ var downloadLink = {
 		'link': 'https://crypto.cat/get/cryptocat.safariextz'
 	},
 	'opera': {
-		'text': 'Unfortunately, Cryptocat is only available for Chrome, Firefox and Safari.',
+		'text': 'Unfortunately, Cryptocat is only available for Chrome, Firefox and Safari',
 		'link': '#'
 	},
 	'internetExplorer': {
-		'text': 'Unfortunately, Cryptocat is only available for Chrome, Firefox and Safari.',
+		'text': 'Unfortunately, Cryptocat is only available for Chrome, Firefox and Safari',
 		'link': '#'
 	}
-}
+};
 
 var mediaQuotes = [
-	'“Cryptocat has a simple, countercultural goal: people should be able to talk on the Internet without being subjected to surveillance.”',
-	'"An essential privacy tool."',
-	'“The beauty of Cryptocat is its simplicity.”',
-	'“One of the best Google Chrome apps currently out there.”',
-	'“We’ve finally found a stylish and secure way to chat online that fits our hip/paranoid lifestyle: Cryptocat.”',
-	'“16th Annual Webby Awards Honoree, Social Media Category.”',
-	'“Wall Street Journal Data Transparency Award for Outstanding Data Control Project.”',
-	'“Cryptocat brings extra-secure communication to web chat, especially in places where conversations might be watched.”',
-	'“Cryptocat may turn out to be the Web’s easiest way to communicate with strong encryption.”',
-	'“Cryptocat creates an encrypted, disposable chatroom on any computer with a web browser.”',
-	'“Cryptocat works inside a web browser and enables people to chat online via encrypted instant messaging.”'
-];
-var mediaSources = [
-	'The New York Times',
-	"Forbes",
-	'PC World',
-	'Business Insider',
-	'Netted',
-	'Webby Awards',
-	'Wall Street Journal',
-	'Ars Technica',
-	'Forbes',
-	'Lifehacker',
-	'BBC News'
+	{
+		'quote': '“Cryptocat has a simple, countercultural goal: people should be able to talk on the Internet without being subjected to surveillance.”',
+		'source': 'The New York Times'
+	},
+	{
+		'quote': '"An essential privacy tool."',
+		'source': 'Forbes'
+	},
+	{
+		'quote': '“The beauty of Cryptocat is its simplicity.”',
+		'source': 'PC World'
+	},
+	{
+		'quote': '“One of the best Google Chrome apps currently out there.”',
+		'source': 'Business Insider'
+	},
+	{
+		'quote': '“We’ve finally found a stylish and secure way to chat online that fits our hip/paranoid lifestyle: Cryptocat.”',
+		'source': 'Netted'
+	},
+	{
+		'quote': '“16th Annual Webby Awards Honoree, Social Media Category.”',
+		'source': 'Webby Awards'
+	},
+	{
+		'quote': '“Wall Street Journal Data Transparency Award for Outstanding Data Control Project.”',
+		'source': 'Wall Street Journal'
+	},
+	{
+		'quote': '“Cryptocat brings extra-secure communication to web chat, especially in places where conversations might be watched.”',
+		'source': 'Ars Technica'
+	},
+	{
+		'quote': '“Cryptocat may turn out to be the Web’s easiest way to communicate with strong encryption.”',
+		'source': 'Forbes'
+	},
+	{
+		'quote': '“Cryptocat creates an encrypted, disposable chatroom on any computer with a web browser.”',
+		'source': 'Lifehacker'
+	},
+	{
+		'quote': '“Cryptocat works inside a web browser and enables people to chat online via encrypted instant messaging.”',
+		'source': 'BBC News'
+	}
 ];
 
 function detectBrowser() {
@@ -72,10 +92,6 @@ function scrollToAnchor(aid){
 	var aTag = $('#' + aid);
 	$('body').animate({scrollTop: aTag.offset().top}, 800);
 }
-
-$('#map').scroll(function(e) {
-	e.preventDefault();
-})
 
 $('#cryptocatLink').click(function(e) {
 	e.preventDefault();
